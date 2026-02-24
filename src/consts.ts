ea21aeef5cc9a2c90cd614ea06ccc9ab756effaf
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2026-02-24 13:58:57 +0000

    feat: cyberpunk style (#2)
    * feat: cyberpunk style
    
    * fix: favicon

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Daniel Williams";
+export const SITE_TITLE = "dannyhw";
 export const SITE_DESCRIPTION = "Welcome to my website!";
